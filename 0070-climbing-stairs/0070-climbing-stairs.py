8a6299b3307c2af767e2d2ce11bd896e9554a189
Sneha_S <snehapriya1801@gmail.com>
2025-12-12 19:56:48 +0530

    Time: 0 ms (100%), Space: 12.2 MB (98.98%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -3,9 +3,8 @@ class Solution(object):
         if n <= 2:
             return n
         
-        a, b = 1, 2   # ways(1)=1, ways(2)=2
-        
+        a, b = 1, 2  
         for i in range(3, n + 1):
-            a, b = b, a + b   # update the last two results
+            a, b = b, a + b   
         
         return b
